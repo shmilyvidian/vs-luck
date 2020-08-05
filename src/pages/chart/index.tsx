611f09d609a8b7fd30eef1f77330aa1378f6bdc4
frosv <616342684@qq.com>
2020-08-05 15:41:19 +0800

    feat: 完成聊天详情页

--- src/pages/chart/index.tsx
+++ src/pages/chart/index.tsx
@@ -1,17 +1,36 @@
 import React, { Component } from 'react'
-import { View, Text } from '@tarojs/components'
+import Taro from "@tarojs/taro";
+import { View, Input, Image, ScrollView } from '@tarojs/components'
 import { observer, inject } from 'mobx-react'
-import { HomeStore } from '@/store/homeStore'
 import './index.scss'
+
+// 图片
+import voice from '@/assets/images/voice.png'
+import add from '@/assets/images/add.png'
+import emoji from '@/assets/images/emoji.png'
+import picImg from '@/assets/images/pic.png'
+import shootImg from '@/assets/images/shoot.png'
+import locationImg from '@/assets/images/location.png'
+
 
 type propsType = {
   store: {
     homeStore,
-  }
+  },
+  userName: string
+}
+
+interface infoListType {
+  date: string | null;
+  info: any;
+  type: string;
 }
 
 type stateType = {
-
+  infoList: Array<infoListType>,
+  inputString: string,
+  scrollInfo: string,
+  showAddInfo: boolean
 }
 
 interface Index {
@@ -22,27 +41,141 @@ interface Index {
 @inject('store')
 @observer
 class Index extends Component {
-  private homeStore: HomeStore
-  constructor(props){
+  constructor(props) {
     super(props)
-    this.homeStore = props.store.homeStore
+    this.state = {
+      infoList: [{
+        date: '12:00',
+        info: '你好，认识一下好不好',
+        type: 'receive'
+      }, {
+        date: '12:04',
+        info: '好鸭好鸭，我叫小雅',
+        type: 'send'
+      }, {
+        date: '',
+        info: '晚上可以一起吃个饭吗？',
+        type: 'receive'
+      }],
+      inputString: '',
+      scrollInfo: '',
+      showAddInfo: false
+    }
   }
-  componentWillMount () { }
 
-  componentDidMount () { }
+  componentDidMount(){
+    // 根据外部传入切换顶部标题
+    Taro.setNavigationBarTitle({
+      title: this.props.userName ? this.props.userName: '聊天'
+    })
+  }
 
-  componentWillUnmount () { }
+  // 输入文字
+  onInputInfo = (e) => {
 
-  componentDidShow () { }
+    console.log(e)
 
-  componentDidHide () { }
+    const { infoList } = this.state
+    infoList.push({
+      date: '',
+      info: e.detail.value,
+      type: 'send'
+    })
 
-  render () {
-    const { name } = this.homeStore
+    infoList.push({
+      date: '',
+      info: (Math.random() * 100).toFixed(2),
+      type: 'receive'
+    })
+
+    // 设置消息 滚动到底部
+    this.setState({
+      infoList,
+      scrollInfo: 'msg' + (infoList.length - 1)
+    })
+  }
+
+  // 是否显示额外功能
+  onAddInfo = () => {
+    let { showAddInfo } = this.state
+    showAddInfo = !showAddInfo
+
+    const { infoList } = this.state
+    this.setState({
+      showAddInfo,
+      scrollInfo: 'msg' + (infoList.length - 1)
+    })
+    // 点击加号 将聊天记录滚动到底部 避免遮挡
+  }
+
+  render() {
+    const { infoList, inputString, scrollInfo, showAddInfo } = this.state
+
+    // 显示信息
+    const infoListTpl = infoList.map((item, index) => {
+      if (item.type === 'receive') {
+        return (
+          <View className="info-item" key={index} id={'msg' + index}>
+            {item.date ? <View className="date-item">{item.date}</View> : null}
+            <View className="receive-msg">
+              {item.info}
+            </View>
+          </View>
+        )
+      } else {
+        return (
+          <View className="info-item" key={index} id={'msg' + index}>
+            {item.date ? <View className="date-item">{item.date}</View> : null}
+            <View className="send-item">
+              <View className="send-msg">
+                {item.info}
+              </View>
+            </View>
+          </View>
+        )
+      }
+    })
+
     return (
       <View className='index'>
-        <Text>{name}</Text>
-      </View>
+        {/* 对话框 */}
+        <ScrollView
+          className={
+            showAddInfo ? "fixed-show info-wrapper" : "info-wrapper"
+          }
+
+          scrollIntoView={scrollInfo}
+          scrollWithAnimation
+          scroll-y
+        >
+          {infoListTpl}
+        </ScrollView>
+        {/* end of 对话框 */}
+        {/* 文字输入 */}
+        <View className="fixed">
+          <View className="input-wrapper">
+            <Image className="voice" src={voice}></Image>
+            <Input
+              className="input"
+              focus
+              value={inputString}
+              adjust-position
+              onConfirm={this.onInputInfo}
+            ></Input>
+            <Image className="emoji" src={emoji}></Image>
+            <Image className="add" src={add} onClick={this.onAddInfo}></Image>
+          </View>
+
+          {/* 判断是否显示功能 */}
+          {showAddInfo ? <View className="other-function">
+            <Image className="function-icon" src={picImg}></Image>
+            <Image className="function-icon" src={shootImg}></Image>
+            <Image className="function-icon" src={locationImg}></Image>
+          </View> : null}
+        </View>
+        {/* end of 文字输入 */}
+
+      </View >
     )
   }
 }
