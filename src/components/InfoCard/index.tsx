9bd0de6de96448171ff426417f69b093623e830a
frosv <616342684@qq.com>
2020-08-06 15:30:16 +0800

    fix:  调整hook写法

--- src/components/InfoCard/index.tsx
+++ src/components/InfoCard/index.tsx
@@ -4,38 +4,36 @@ import { InfoCardBox, LeftIn } from './indexSty'
 import { EmptyView } from '../EmptyView'
 
 type Icard = {
-    name: string
-    time: string
-    info: string
+  name: string
+  time: string
+  info: string
 }
 
 interface IProps {
-    cardData: Icard[]
+  cardData: Icard[]
 }
 
 export function InfoCard(props: IProps) {
 
-    render() {
-        const { cardData = [] } = this.props
-        return (
-            <View>
-                {
-                    cardData.length ?
-                        cardData.map((o, i) =>
-                            <LeftIn key={i} left={-i}>
-                                <InfoCardBox>
-                                    <View className="card_top">
-                                        <Text className="card_top_name">{o.name}</Text>
-                                        <Text className="card_top_time">{o.time}</Text>
-                                    </View>
-                                    <Text className="card_bottom">{o.info}
-                                    </Text>
-                                </InfoCardBox>
-                            </LeftIn>
-                        ) :
-                        <EmptyView text="啊哦，还没有好姻缘快去匹配发现一下吧" />
-                }
-            </View>
-        )
-    }
+  const { cardData = [] } = props
+  return (
+    <View>
+      {
+        cardData.length ?
+          cardData.map((o, i) =>
+            <LeftIn key={i} left={-i}>
+              <InfoCardBox>
+                <View className="card_top">
+                  <Text className="card_top_name">{o.name}</Text>
+                  <Text className="card_top_time">{o.time}</Text>
+                </View>
+                <Text className="card_bottom">{o.info}
+                </Text>
+              </InfoCardBox>
+            </LeftIn>
+          ) :
+          <EmptyView text="啊哦，还没有好姻缘快去匹配发现一下吧" />
+      }
+    </View>
+  )
 }
